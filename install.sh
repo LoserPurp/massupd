84a145cd4408c207fcf2eb13285a2c3833c67139
Robin <99472938+LoserPurp@users.noreply.github.com>
2024-05-23 20:39:48 +0200

    Bug fix
    - added check if installer is running as root
    - script now makes wrapper executable

--- install.sh
+++ install.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-# Navigate to the massupd folder
-cd massupd || { echo "Failed to change directory to massupd"; exit 1; }
+#Checks if the script is run as root
+if [ "$EUID" -ne 0 ]
+  then echo "You need to run the install script as root"
+  exit
+fi
 
-# Install pip requirements
+#Navigates to the massupd folder
+cd massupd || { echo "Failed to change directory to massupd (You need to run this script outside the massupd folder)"; exit 1; }
+
+#Installs pip requirements
 if [ -f requirements.txt ]; then
   pip install -r requirements.txt || { echo "Failed to install pip requirements"; exit 1; }
 else
@@ -11,16 +17,19 @@ else
   exit 1
 fi
 
-# Move massupd script to /usr/bin/massupd
+#Moves massupd script to /usr/bin/massupd
 if [ -f massupd ]; then
-  sudo mv massupd /usr/bin/massupd || { echo "Failed to move massupd script to /usr/bin"; exit 1; }
+  mv massupd /usr/bin/massupd || { echo "Failed to move massupd script to /usr/bin"; exit 1; }
 else
   echo "massupd script not found"
   exit 1
 fi
 
-# Move the massupd folder to /usr/lib/massupd
+#Moves the massupd folder to /usr/lib/massupd
 cd .. || { echo "Failed to change directory to parent"; exit 1; }
-sudo mv massupd /usr/lib/massupd || { echo "Failed to move massupd folder to /usr/lib"; exit 1; }
+mv massupd /usr/lib/massupd || { echo "Failed to move massupd folder to /usr/lib"; exit 1; }
+
+#Makes script executeable
+chmod +r /usr/bin/massupd
 
 echo "Installation completed successfully"
